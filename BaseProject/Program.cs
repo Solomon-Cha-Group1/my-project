45cfc1bfad3a09cb99b7f6232778c41c3ea3c4e3
chaya <0001.chaya@gmail.com>
2020-10-19 10:59:50 +0300

    base commit

---
+++ BaseProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Algorithm;
+using ReadAndWriteData;
+
+namespace BaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           ReadAndWriteData.Class1 c = new ReadAndWriteData.Class1();
+            c.wr();
+            Console.WriteLine("version2");
+        }
+    }
+}
